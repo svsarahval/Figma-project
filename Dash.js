953879fdd2679c1d449bb118a26e9b6cc3eb3bff
svsarahval <svsarahval@gmail.com>
2023-01-27 05:21:14 -0500

    fourth or fifth try to get the logIn to work

--- Dash.js
+++ Dash.js
@@ -3,7 +3,7 @@ const userInput = document.getElementById('userInput');
 const userPostBtn = document.getElementById('userPostBtn');
 
 function userPostHandler(event) {
-  debugger;
+  // debugger;
   event.preventDefault();
   event.stopPropagation();
   const userPost = userInput.value;
@@ -15,3 +15,12 @@ function userPostHandler(event) {
   }
 }
 userPostBtn.addEventListener('click', userPostHandler);
+
+// function deletePost(post) {let postList = document.querySelector('.post-list');postList.removeChild(post.parentNode);}function editPost(post) {// get new post text    var newPostText = prompt("Enter new post text:");    // update post text    post.innerHTML = newPostText;  }
+
+// const btnForUserPost = document.createElement('div');
+// btnForUserPost.classList.add('actionBtn');
+// btnForUserPost.innerHTML ='
+// <button onclick = "deleteUserPost(this)" type="submit" class="deleteBtn">Delete</button>
+// '
+// <button onclick = "editUserPost(this)" type="button" class="editBtn">Edit</button>;
