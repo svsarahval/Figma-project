d9beb484a4ba9d013e43d51e66122620f191f26a
svsarahval <svsarahval@gmail.com>
2023-01-26 22:05:25 -0500

    trying trevor's suggestion not working

--- login.js
+++ login.js
@@ -1,28 +1,43 @@
-$(document).ready(function() {
-  let userData = [
-  {
-    email: 'svsarahval@gmail.com',
-    password: 'password',
-  },
-  {
-    email: 'KLat@gmail.com',
-    password: 'password',
-  },
-  {
-    email: 'kikijohnson@gmail.com',
-    password: 'password',
-  },
-  {
-    email: 'NathTate2@gmail.com',
-    password: 'password',
-  },
-];
-
 const password = document.getElementById('password');
 const email = document.getElementById('email');
 const loginForm = document.getElementById('loginForm');
 const signUpBtn = document.getElementById('signUpBtn');
 const logInBtn = document.getElementById('logInBtn');
+
+  let userData = [
+    {
+      email: 'svsarahval@gmail.com',
+      password: 'password',
+    },
+    {
+      email: 'KLat@gmail.com',
+      password: 'password',
+    },
+    {
+      email: 'kikijohnson@gmail.com',
+      password: 'password',
+    },
+    {
+      email: 'NathTate2@gmail.com',
+      password: 'password',
+    },
+  ];
+
+  function handleFormSubmit(){
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Making sign up  go to Sign up page
 signUpBtn.addEventListener('click', gotoSignUp);
@@ -39,24 +54,24 @@ function gotoSignUp(e) {
 // }
 
 // validate email and password
-let emailValue = email.value;
-let passwordValue = password.value;
+// let emailValue = email.value;
+// let passwordValue = password.value;
 
-loginForm.addEventListener('click', gotoDash);
+// loginForm.addEventListener('click', gotoDash);
 
-function gotoDash(e) {
-  e.preventDefault();
-  let isValid = false;
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].email === emailValue && users[i].password === passwordValue) {
-      isValid = true;
-      break;
-    }
-  }
+// function gotoDash(e) {
+//   e.preventDefault();
+//   let isValid = false;
+//   for (let i = 0; i < users.length; i++) {
+//     if (users[i].email === emailValue && users[i].password === passwordValue) {
+//       isValid = true;
+//       break;
+//     }
+//   }
 
-  if ((isValid /= true)) {
-    alert('Invalid email or password');
-  } else {
-    window.location.href = 'Dash.html';
-  }
+//   if ((isValid /= true)) {
+//     alert('Invalid email or password');
+//   } else {
+//     window.location.href = 'Dash.html';
+//   }
 }
