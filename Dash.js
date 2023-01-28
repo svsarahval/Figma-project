dda336cad74dea30759bc1b15541b6e7aa5fe1cc
svsarahval <svsarahval@gmail.com>
2023-01-28 08:05:50 -0500

    Dash Is WORKING!

--- Dash.js
+++ Dash.js
@@ -1,43 +1,84 @@
 const userPost = document.getElementById('userPost');
 const userInput = document.getElementById('userInput');
 const userPostBtn = document.getElementById('userPostBtn');
+const container4posts = document.querySelector('#container4posts');
 
 function userPostHandler(event) {
-  // debugger;
   event.preventDefault();
   event.stopPropagation();
   const userPost = userInput.value;
   if (userPost) {
-    const container4posts = document.querySelector('#container4posts');
+    const newPost = document.createElement('div');
+    newPost.innerHTML = `
+      <p>${userPost}</p>
+      <button class="edit-btn">Edit</button>
+      <button class="delete-btn">Delete</button>
+    `;
+    container4posts.prepend(newPost);
+
+    const editBtn = newPost.querySelector('.edit-btn');
+    editBtn.addEventListener('click', function () {
+      const postText = newPost.querySelector('p');
+      const textArea = document.createElement('textarea');
+      textArea.value = postText.innerText;
+      newPost.replaceChild(textArea, postText);
+      editBtn.innerText = 'Save';
+      editBtn.removeEventListener('click', arguments.callee);
+      editBtn.addEventListener('click', function () {
+        postText.innerText = textArea.value;
+        newPost.replaceChild(postText, textArea);
+        editBtn.innerText = 'Edit';
+        editBtn.removeEventListener('click', arguments.callee);
+        editBtn.addEventListener('click', function () {
+          const textArea = document.createElement('textarea');
+          textArea.value = postText.innerText;
+          newPost.replaceChild(textArea, postText);
+          editBtn.innerText = 'Save';
+          editBtn.removeEventListener('click', arguments.callee);
+          editBtn.addEventListener('click', function () {
+            postText.innerText = textArea.value;
+            newPost.replaceChild(postText, textArea);
+            editBtn.innerText = 'Edit';
+            editBtn.removeEventListener('click', arguments.callee);
+            editBtn.addEventListener('click', arguments.callee);
+          });
+        });
+      });
+    });
+    const deleteBtn = newPost.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', function () {
+      newPost.remove();
+    });
   }
 }
-const newPost = document.createElement('div');
-newPost.innerText = userPost;
-container4posts.prepend(newPost);
-
 userPostBtn.addEventListener('click', userPostHandler);
 
-const editBtn = document.createElement('button');
-editBtn.innerText = 'Edit';
-editBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  userInput.value = newPost.innerText;
-});
-newPost.prepend(editBtn);
+// function userPostHandler(event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//   const userPost = userInput.value;
+//   if (userPost) {
+//     const newPost = document.createElement('div');
+//     newPost.innerText = userPost;
 
-const deleteBtn = document.createElement('button');
-deleteBtn.innerText = 'Delete';
-deleteBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  newPost.remove();
-});
-newPost.prepend(deleteBtn);
+//     // Adding edit button
+//     const editBtn = document.createElement('button');
+//     editBtn.innerText = 'Edit';
+//     editBtn.addEventListener('click', function () {
+//       userInput.value = userPost;
+//       container4posts.removeChild(newPost);
+//     });
+//     newPost.appendChild(editBtn);
 
-// function deletePost(post) {let postList = document.querySelector('.post-list');postList.removeChild(post.parentNode);}function editPost(post)  get new post text var newPostText = prompt("Enter new post text:"); update post text post.innerHTML = newPostText;}
+//     // Adding delete button
+//     const deleteBtn = document.createElement('button');
+//     deleteBtn.innerText = 'Delete';
+//     deleteBtn.addEventListener('click', function () {
+//       container4posts.removeChild(newPost);
+//     });
+//     newPost.appendChild(deleteBtn);
 
-// const btnForUserPost = document.createElement('div');
-// btnForUserPost.classList.add('actionBtn');
-// btnForUserPost.innerHTML ='
-// <button onclick = "deleteUserPost(this)" type="submit" class="deleteBtn">Delete</button>
-// '
-// <button onclick = "editUserPost(this)" type="button" class="editBtn">Edit</button>;
+//     container4posts.prepend(newPost);
+//   }
+// }
+// userPostBtn.addEventListener('click', userPostHandler);
--- login.js
+++ login.js
@@ -4,60 +4,60 @@ const loginForm = document.getElementById('loginForm');
 const signUpBtn = document.getElementById('signUpBtn');
 const logInBtn = document.getElementById('logInBtn');
 
-const USERS_URL =
-  'https://login-page-sociable-default-rtdb.firebaseio.com/users';
+// const USERS_URL =
+//   'https://login-page-sociable-default-rtdb.firebaseio.com/users';
 
-const POSTS_URL =
-  'https://login-page-sociable-default-rtdb.firebaseio.com/posts';
-const EXT = '.json';
+// const POSTS_URL =
+//   'https://login-page-sociable-default-rtdb.firebaseio.com/posts';
+// const EXT = '.json';
 
-loginForm.addEventListener('submit', (e) => {
-  let messages = [];
+// loginForm.addEventListener('submit', (e) => {
+//   let messages = [];
 
-  if (email.value === '' || email.value == null) {
-    messages.push('Email is required');
-  }
+//   if (email.value === '' || email.value == null) {
+//     messages.push('Email is required');
+//   }
 
-  if (password.value === '' || password.value == null) {
-    messages.push('Password is required');
-  }
+//   if (password.value === '' || password.value == null) {
+//     messages.push('Password is required');
+//   }
 
-  if (
-    password.value.length <= 6 &&
-    (password.value !== '' || password.value !== null)
-  ) {
-    messages.push('Password must be longer than 6 characters');
-  }
+//   if (
+//     password.value.length <= 6 &&
+//     (password.value !== '' || password.value !== null)
+//   ) {
+//     messages.push('Password must be longer than 6 characters');
+//   }
 
-  //   if (messages.length > 0) {e.preventDefault()errorElement.innerText = messages.join(', ')
-  // }
-});
+//   //   if (messages.length > 0) {e.preventDefault()errorElement.innerText = messages.join(', ')
+//   // }
+// });
 
-var database = firebase.database();
-var ref = database.ref(
-  'https://login-page-sociable-default-rtdb.firebaseio.com/'
-);
+// var database = firebase.database();
+// var ref = database.ref(
+//   'https://login-page-sociable-default-rtdb.firebaseio.com/'
+// );
 
-ref.on(
-  'value',
-  function (snapshot) {
-    console.log(snapshot.val());
-  },
-  function (error) {
-    console.log('Error: ' + error.code);
-  }
-);
+// ref.on(
+//   'value',
+//   function (snapshot) {
+//     console.log(snapshot.val());
+//   },
+//   function (error) {
+//     console.log('Error: ' + error.code);
+//   }
+// );
 
-// // Making sign up  go to Sign up page
-// signUpBtn.addEventListener('click', gotoSignUp);
-// function gotoSignUp(e) {
-//   e.preventDefault();
-//   window.location.replace('/signUp.html');
-// }
-// // Making Sign In go to Dash page
-// logInBtn.addEventListener('click', gotoDash);
-// function gotoDash(event) {
-//   event.preventDefault();
-//   window.location.replace('/Dash.html');
-//   alert('working');
-// }
+// Making sign up  go to Sign up page
+signUpBtn.addEventListener('click', gotoSignUp);
+function gotoSignUp(e) {
+  e.preventDefault();
+  window.location.replace('/signUp.html');
+}
+// Making Sign In go to Dash page
+logInBtn.addEventListener('click', gotoDash);
+function gotoDash(event) {
+  event.preventDefault();
+  window.location.replace('/Dash.html');
+  alert('working');
+}
