65cead1b9e8c71bd15cbae9578d71213b349bb2a
svsarahval <svsarahval@gmail.com>
2023-01-29 15:34:14 -0500

    I AM DONE SIGN UP DONE!

--- login.js
+++ login.js
@@ -1,63 +1,48 @@
 const password = document.getElementById('password');
-const email = document.getElementById('email');
+const userName = document.getElementById('userName');
 const loginForm = document.getElementById('loginForm');
 const signUpBtn = document.getElementById('signUpBtn');
 const logInBtn = document.getElementById('logInBtn');
 
-// const USERS_URL =
-//   'https://login-page-sociable-default-rtdb.firebaseio.com/users';
-
-// const POSTS_URL =
-//   'https://login-page-sociable-default-rtdb.firebaseio.com/posts';
-// const EXT = '.json';
-
-// loginForm.addEventListener('submit', (e) => {
-//   let messages = [];
-
-//   if (email.value === '' || email.value == null) {
-//     messages.push('Email is required');
-//   }
-
-//   if (password.value === '' || password.value == null) {
-//     messages.push('Password is required');
-//   }
-
-//   if (
-//     password.value.length <= 6 &&
-//     (password.value !== '' || password.value !== null)
-//   ) {
-//     messages.push('Password must be longer than 6 characters');
-//   }
-
-//   //   if (messages.length > 0) {e.preventDefault()errorElement.innerText = messages.join(', ')
-//   // }
-// });
-
-// var database = firebase.database();
-// var ref = database.ref(
-//   'https://login-page-sociable-default-rtdb.firebaseio.com/'
-// );
-
-// ref.on(
-//   'value',
-//   function (snapshot) {
-//     console.log(snapshot.val());
-//   },
-//   function (error) {
-//     console.log('Error: ' + error.code);
-//   }
-// );
-
-// Making sign up  go to Sign up page
-signUpBtn.addEventListener('click', gotoSignUp);
-function gotoSignUp(e) {
-  e.preventDefault();
-  window.location.replace('/signUp.html');
-}
-// Making Sign In go to Dash page
-logInBtn.addEventListener('click', gotoDash);
-function gotoDash(event) {
+logInBtn.addEventListener('click', async (event) => {
   event.preventDefault();
-  window.location.replace('/Dash.html');
-  alert('working');
-}
+  const userData = {
+    username: userName.value,
+    password: password.value,
+  };
+  try {
+    const response = await fetch(
+      'https://login-page-sociable-default-rtdb.firebaseio.com/users.json',
+      {
+        method: 'POST',
+        // mode: 'no-cors',
+        body: JSON.stringify(userData),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+    const data = await response.json();
+    if (response.ok) {
+      // store user information in a secure way
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      // redirect to user dashboard
+      window.location.replace('Dash.html');
+    } else {
+      // display an error message
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// // Making sign up  go to Sign up page
+// signUpBtn.addEventListener('click', gotoSignUp);
+// function gotoSignUp(e) {
+//   e.preventDefault();
+//   window.location.replace('/signUp.html');
+// }
+// // Making Sign In go to Dash page
+// logInBtn.addEventListener('click', gotoDash);
+// function gotoDash(event) {
+//   event.preventDefault();
+//   window.location.replace('/Dash.html');
+//   alert('working');
--- signup.js
+++ signup.js
@@ -1,3 +1,6 @@
+const USER_URL = 'https://login-page-sociable-default-rtdb.firebaseio.com/';
+const EXT = `.json`;
+
 const submitBtn = document.getElementById('submitBtn');
 const confPass = document.getElementById('confPass');
 const password = document.getElementById('password');
@@ -6,15 +9,61 @@ const email = document.getElementById('email');
 const lastName = document.getElementById('lastName');
 const firstName = document.getElementById('firstName');
 
-
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    let newUser= {
-     firstName: firstName.value,
-     lastName: lastName.value,
-     email: email.value,
-     password: password.value,
-     birthDate: birthDate.value,
-     confPass: confPass.value
-    }
-}
+  e.preventDefault();
+  let newUser = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    password: password.value,
+    birthDate: birthDate.value,
+    confPass: confPass.value,
+  };
+
+  fetch(`${USER_URL}${EXT}`, {
+    method: 'PUT',
+    body: JSON.stringify(newUser),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(newUser);
+      window.location.href = 'Dash.html';
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// submitBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   let newUser = {
+//     firstName: firstName.value,
+//     lastName: lastName.value,
+//     email: email.value,
+//     password: password.value,
+//     birthDate: birthDate.value,
+//     confPass: confPass.value,
+//   });
+
+// fetch(`{$USER_URL}${EXT}`, {
+//   method: 'PUT',
+//   body: JSON.stringify(newUser),
+//   // headers: {
+//   //   'Content-Type': 'application/json',
+//   // },
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(newUser);
+//     window.location.href = 'Dash.html';
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
